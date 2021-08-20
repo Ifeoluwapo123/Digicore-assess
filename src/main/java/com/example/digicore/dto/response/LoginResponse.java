d6ed4caac0831a19c1fad7800bcad827b05009aa
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-08-20 01:20:26 +0100

    login response handler

---
+++ src/main/java/com/example/digicore/dto/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.example.digicore.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+   private boolean success;
+   private String accessToken;
+}
